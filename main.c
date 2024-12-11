b1294812235b7d84354789d235986648f5b1ff40
Valentino <entinosusini@gmail.com>
2024-12-11 10:23:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -40,15 +40,6 @@ static void	ft_insert_int_to_top(t_stack *stack, char c, int number)
 	}
 }
 
-/**
- * @brief Function used to complete the algorithm.
- * Inserts to top the two numbers in parameters and pushing them to a.
- * 
- * @param stack_a		pointer to the stack a
- * @param stack_b		pointer to the stack b
- * @param first			the first number to push
- * @param second		the second number to push
- */
 static void	ft_insert_and_push(t_stack *stack_a, t_stack *stack_b, \
 	int first, int second)
 {
@@ -58,15 +49,6 @@ static void	ft_insert_and_push(t_stack *stack_a, t_stack *stack_b, \
 	ft_do_pa(stack_a, stack_b);
 }
 
-/**
- * @brief Function used to push everything from the stack b, to
- * the stack a.
- * It'll get the two biggest numbers and will push the one that
- * is the closest to the top of stack b first then the second one.
- * 
- * @param stack_a 		pointer to the stack a
- * @param stack_b 		pointer to the stack b
- */
 static void	ft_push_b_to_a(t_stack *stack_a, t_stack *stack_b)
 {
 	int	first;
@@ -93,14 +75,6 @@ static void	ft_push_b_to_a(t_stack *stack_a, t_stack *stack_b)
 	}
 }
 
-/**
- * @brief Quick sort algorithm based function, called chunk sort here.
- * It'll creates small stacks of chunk_size integers.
- * 
- * @param stack_a 		pointer to the stack a
- * @param stack_b 		pointer to the stack b
- * @param chunk_size 	the size of a chunk
- */
 void	ft_chunk_sort(t_stack *stack_a, t_stack *stack_b, int chunk_size)
 {
 	int	index;
@@ -145,13 +119,6 @@ int	ft_get_index_of_int(t_stack *stack, int number)
 	return (-1);
 }
 
-/**
- * @brief Function used to find the next max int in a stack.
- * 
- * @param stack 		pointer to the stack
- * @param max 			the current max int in the stack
- * @return int 			the next max int found
- */
 int	ft_find_next_max_int(t_stack *stack, int max)
 {
 	int	i;
@@ -170,13 +137,6 @@ int	ft_find_next_max_int(t_stack *stack, int max)
 	return (next_max);
 }
 
-/**
- * @brief Function used to find the next min int in a stack.
- * 
- * @param stack 		pointer to the stack
- * @param min 			the current min int in the stack
- * @return int 			the next min int found
- */
 int	ft_find_next_min_int(t_stack *stack, int min)
 {
 	int	i;
@@ -195,14 +155,6 @@ int	ft_find_next_min_int(t_stack *stack, int min)
 	return (next_min);
 }
 
-/**
- * @brief Function used to found the first number's index that
- * is smaller than the min int in a stack.
- * 
- * @param stack 		pointer to the stack
- * @param min 			the current min int in the stack
- * @return int 			the index of the first number smaller than min
- */
 int	ft_get_min_int_index(t_stack *stack, int min)
 {
 	int	i;
@@ -225,11 +177,6 @@ static void	ft_sort_two(t_stack *stack)
 		ft_do_rotate(stack, 'a');
 }
 
-/**
- * @brief Simple brute forced algorithm for three numbers
- * 
- * @param stack 		pointer to the stack
- */
 static void	ft_sort_three(t_stack *stack)
 {
 	if (ft_is_sorted(stack))
@@ -253,12 +200,6 @@ static void	ft_sort_three(t_stack *stack)
 	}
 }
 
-/**
- * @brief Simple brute forced algorithm for four or five numbers
- * 
- * @param stack_a 			pointer to the stack a
- * @param stack_b 			pointer to the stack b
- */
 static void	ft_sort_four_and_five(t_stack *stack_a, t_stack *stack_b)
 {
 	if (ft_is_sorted(stack_a))
@@ -284,14 +225,6 @@ static void	ft_sort_four_and_five(t_stack *stack_a, t_stack *stack_b)
 	}
 }
 
-/**
- * @brief Function used to initialize the sorting algorithms.
- * It'll find the best algorithm to start depending on the size
- * of the stack a.
- * 
- * @param stack_a 			pointer to the stack a
- * @param stack_b 			pointer to the stack b
- */
 void	ft_init_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	if (!stack_a || !stack_b)
